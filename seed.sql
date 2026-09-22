dacb45b93be227af32d818acc493c3b1e6bd7f94
Dhananjay Mohan <dhananjaymohan67@gmail.com>
2026-09-22 20:31:40 +0530

    new changes

--- seed.sql
+++ seed.sql
@@ -1,1 +1,2 @@
-INSERT OR IGNORE INTO users (id, email, password_hash, role, created_at) VALUES ('usr-admin-001', 'admin@kl01carspa.com', '$2b$10$e4GfTz84ep49GQtFIMsNHup5hPo9wctaljTcluKyG0.jqdLnuTmGe', 'ADMIN', datetime('now'));
+INSERT OR IGNORE INTO users (id, email, password_hash, role, created_at) VALUES ('usr-admin-001', 'admin@kl01carspa.com', '$2b$10$lIzE7QfAi9X8WnDyUy4z6e4QbDlsM.l.2C0XYv10URxXH.IIO1zva', 'ADMIN', datetime('now'));
+
